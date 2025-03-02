8d75aea2aa2efb3b8ba556a86d6d927f03a6e207
devMapute <ammapute1@up.edu.ph>
2025-03-02 12:40:57 +0800

    Add navigation button to About page and implement user route for GET and POST requests

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { useRouter } from "next/navigation";
 
 export default function About() {
+  const router = useRouter();
   return (
     <div>
-      
+      <button
+        className="bg-amber-100 text-black rounded-2xl"
+        onClick={() => router.push("/")}
+      >
+        <div className="p-2">Go home</div>
+      </button>
     </div>
-  )
+  );
 }
